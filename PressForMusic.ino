5c2393cea0d3a7dcc2b1eccb32bd6d07a397a98e
Tyler Gunn <tyler@egunn.com>
2020-10-13 21:04:52 -0700

    Add support for a "pause relay"
    Add ability to have a relay turn on when a "pause" element is hit in your playlist.  Maybe to light up a tune to sign or something like that.

--- PressForMusic.ino
+++ PressForMusic.ino
@@ -22,14 +22,18 @@
 // It is assumed that you're using DHCP to get an IP address; there is no strict need to have a
 // static IP for this application.
 
-#define WIFI_SSID "supercoolssid"
-#define WIFI_PASSWORD "supersecure"
+#define WIFI_SSID "booga"
+#define WIFI_PASSWORD "booga"
 
 // ------------------------------------------------------------------------------------------------
 // This section contains settings for the MQTT server you wish the device to connect to.
 
 #define MQTT_SERVER_IP IPAddress(192, 168, 0, 125)
 #define MQTT_SERVER_PORT 1883
+
+
+// ------------------------------------------------------------------------------------------------
+// This section contains MQTT topic definitions.  You shouldn't need to look into this much.
 
 // This is the topic we will subscribe to on which we expect to receive basic idle/playing indications.
 #define MQTT_FPP_STATUS_TOPIC "christmas/falcon/player/FPP/status"
@@ -40,6 +44,10 @@
 // This is the topic we will subscribe to for the purpose of determining which song within the playlist is
 // currently playing.
 #define MQTT_FPP_PLAYLIST_POSITION_TOPIC "christmas/falcon/player/FPP/playlist/sectionPosition/status"
+
+// This is the topic we will subscribe to for the purpose of determining the name of the media file
+// currently playing.
+#define MQTT_FPP_PLAYLIST_SEQUENCE_STATUS_TOPIC "christmas/falcon/player/FPP/playlist/sequence/status"
 
 // We will publish push events to this topic when there is music playing; this can be used to get a
 // count of how many people press the button during your show.
@@ -58,17 +66,17 @@
 // Configuration below this line should not need to be changed if you setup the device in the
 // usual manner.
 
-// This is the pin on your ESP8266 which has a relay connected to it.
+// This is the pin on your ESP8266 which has a relay connected to it which controls your
+// speakers.
 // I used some cheap 3V 1 channel opto-isolated relays.
 // https://www.amazon.com/gp/product/B07ZM84BVX
 // These were designed specically for high level triggering from an
 // ESP8266 DIO pin.
-#define RELAY_PIN D1   
+#define SPEAKER_RELAY_PIN D1
 
 // This is the pin on your ESP8233 which has the momentary switch connected to it.
 // I used an industrial foot switch for mine:
 // https://www.amazon.com/gp/product/B00EF9D2DY
-
 #define SWITCH_PIN D5  
 
 // When set to 1, indicates that the speakers shall be active until the
@@ -77,6 +85,29 @@
 // end of the next song.
 // ... etc
 #define SONG_COUNT 2   
+
+// ------------------------------------------------------------------------------------------------
+// This section is for configuration of triggering another relay pin during times in your
+// playlist when no media is playing.
+// You may, for example, want to turn on a light during "Pause" entries in your playlist.
+// Note: This isn't perfect; FPP doesn't actually publish that a "pause" entry is being played,
+// so the closest proxy is to monitor for times when no media is playing.
+// By default this functionality is turned off
+
+#define TRIGGER_RELAY_DURING_PAUSE
+
+// This is the pin on your ESP8266 which has a relay connected to it for the purpose of triggering
+// during media pauses.
+#define PAUSE_RELAY_PIN D2
+
+// This is how long AFTER a pause ends until the relay will be turned off again.
+// Unit is milliseconds.  The default value of 3000 means that 3 seconds after a pause and media
+// starts playing again, we'll turn the relay off.
+#define RELAY_OFF_AFTER_PAUSE_DURATION_MILLIS 3000
+
+
+// ------------------------------------------------------------------------------------------------
+// Constants.  Yay.
 
 // Status expected on the MQTT_FPP_STATUS_TOPIC topic when nothing is playing.
 #define FPP_STATUS_IDLE "idle"
@@ -94,6 +125,11 @@ Ticker wifiReconnectTimer;
 // Whether we know FPP is playing music or not.
 bool isFppPlaying = false;
 
+#define PAUSE_STATUS_PAUSED 0
+#define PAUSE_STATUS_UNPAUSED 1
+#define PAUSE_STATUS_UNPAUSE_SCHEDULED 2
+volatile int pauseStatus = PAUSE_STATUS_UNPAUSED;
+
 // We use the current position in the playlist to know when songs change.
 int currentPlaylistPosition = -1;
 // We track the current button state and do some debouncing to ensure we get a stable
@@ -108,13 +144,14 @@ int songsRemaining = -1;
 // Last time the button changed state; used with below to debounce input.
 unsigned long lastButtonStateChangeTime = 0;
 unsigned long debounceTime = 50;
+unsigned long deactivePauseRelayTime = 0;
 
 // Shut off the speaker relay.
 void deactiveSpeakers() {
   #ifdef DEBUG_SERIAL
     Serial.println("Deactivate speakers");
   #endif
-  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(SPEAKER_RELAY_PIN, LOW);
   songsRemaining = -1;
 }
 
@@ -161,6 +198,9 @@ void onMqttConnect(bool sessionPresent) {
   #endif
   uint16_t packetIdSub = mqttClient.subscribe(MQTT_FPP_STATUS_TOPIC, 2);
   packetIdSub = mqttClient.subscribe(MQTT_FPP_PLAYLIST_POSITION_TOPIC, 2);
+  #ifdef TRIGGER_RELAY_DURING_PAUSE
+  packetIdSub = mqttClient.subscribe(MQTT_FPP_PLAYLIST_SEQUENCE_STATUS_TOPIC, 2);
+  #endif
 }
 
 // Async callback received when connection to MQTT server is lost.
@@ -209,6 +249,15 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
       isFppPlaying = false;
       currentPlaylistPosition = -1;
       deactiveSpeakers();
+      #ifdef TRIGGER_RELAY_DURING_PAUSE
+        if (pauseStatus != PAUSE_STATUS_UNPAUSED) {
+          pauseStatus = PAUSE_STATUS_UNPAUSED;
+          digitalWrite(PAUSE_RELAY_PIN, LOW);
+          #ifdef DEBUG_SERIAL
+            Serial.println("  Turning off pause relay.");
+          #endif
+        }
+      #endif
     } else if (strncmp(payload, FPP_STATUS_PLAYING, len) == 0) {
       #ifdef DEBUG_SERIAL
         Serial.println("  << FPP Is playing");
@@ -244,18 +293,52 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
         deactiveSpeakers();
       }
     }
+  // Change in media playing
+  } else if (strcmp(topic, MQTT_FPP_PLAYLIST_SEQUENCE_STATUS_TOPIC) == 0) {
+    char tempMedia[len + 1];
+    strncpy(tempMedia, payload, len);
+    tempMedia[len] = '\0';
+
+    #ifdef DEBUG_SERIAL
+    Serial.print("  << FPP sequence play status:|");
+    Serial.print(tempMedia);
+    Serial.println("|");
+    #endif
+    bool isSequenceNowPaused = (strlen(tempMedia) == 0);
+    if (pauseStatus == PAUSE_STATUS_UNPAUSED && isSequenceNowPaused) {
+      // No media name, so assume media is paused; turn on the relay
+      pauseStatus = PAUSE_STATUS_PAUSED;
+      digitalWrite(PAUSE_RELAY_PIN, HIGH);
+      #ifdef DEBUG_SERIAL
+        Serial.println("  Media is paused; turn on relay.");
+      #endif
+    } else if (pauseStatus == PAUSE_STATUS_PAUSED && !isSequenceNowPaused) {
+      // In the main loop we will turn off the relay after we hit this timeout.
+      pauseStatus = PAUSE_STATUS_UNPAUSE_SCHEDULED;
+      deactivePauseRelayTime = millis() + RELAY_OFF_AFTER_PAUSE_DURATION_MILLIS;
+      #ifdef DEBUG_SERIAL
+        Serial.print("  Media unpause; schedule relay off at ");
+        Serial.println(deactivePauseRelayTime);
+      #endif
+    }
   }
+  
 }
 
 // Set things up.
 void setup() {
   // Setup the relay and switch pins.
-  pinMode(RELAY_PIN, OUTPUT);
-  pinMode(SWITCH_PIN, INPUT);
-
+  pinMode(SPEAKER_RELAY_PIN, OUTPUT);
   // Let's just be sure that the relay is off on boot.
-  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(SPEAKER_RELAY_PIN, LOW);
+  
+  #ifdef TRIGGER_RELAY_DURING_PAUSE
+  pinMode(PAUSE_RELAY_PIN, OUTPUT);
+  digitalWrite(PAUSE_RELAY_PIN, LOW);
+  #endif
 
+  pinMode(SWITCH_PIN, INPUT);
+  
   Serial.begin(115200);
   Serial.println();
   Serial.println();
@@ -281,7 +364,7 @@ void handleButtonDown() {
       Serial.print("Activate speakers - songs remaining: ");
       Serial.println(songsRemaining);
     #endif
-    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(SPEAKER_RELAY_PIN, HIGH);
     mqttClient.publish(MQTT_BUTTON_PRESS_COUNT_PLAYING_TOPIC, 2, true, "trigger");
     #ifdef MQTT_PUBLISH_TO_BLUEIRIS
       mqttClient.publish(MQTT_BLUEIRIS_TRIGGER_TOPIC, 2, true, MQTT_BLUEIRIS_TRIGGER_PAYLOAD);
@@ -322,4 +405,15 @@ void loop() {
     currentButtonState = newButtonState;
   } 
   lastButtonState = newButtonState;
+
+  #ifdef TRIGGER_RELAY_DURING_PAUSE
+    if (isFppPlaying && pauseStatus == PAUSE_STATUS_UNPAUSE_SCHEDULED && millis() > deactivePauseRelayTime) {
+      #ifdef DEBUG_SERIAL
+        Serial.print("  Media unpause relay off at ");
+        Serial.println(millis());
+      #endif
+      pauseStatus = PAUSE_STATUS_UNPAUSED;
+      digitalWrite(PAUSE_RELAY_PIN, LOW);
+    }
+  #endif
 }
