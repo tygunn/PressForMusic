bc841b5f6e6ca517f339e64a28bc91653b3db610
Tyler Gunn <tyler@egunn.com>
2023-12-26 21:25:47 -0800

    Add HomeAssistant MQTT Auto-discovery support.
    Removed the "rick roll" code; there is a slight chance you can trigger
    the button midshow if you're using Remote Falcon because FPP goes to an
    idle state momentarily.  That means the rick roll sequence plays midshow
    and messes up your main sequence.  It just isn't worth it to try and fix
    that issue.
    
    Also, removed the "trigger relay during pause" code since it was unusued.
    
    Finally, added HomeAssistant MQTT auto-discovery support to:
    1. Report the current button press status.
    2. Show whether the speakers are on.
    3. Allow for override/force-on speakers.

--- PressForMusic.ino
+++ PressForMusic.ino
@@ -27,6 +27,7 @@
 #include <WiFiUdp.h>
 #include <time.h>
 #endif
+#include <ArduinoHA.h> // For Home Assistant Integration
 
 
 // ------------------------------------------------------------------------------------------------
@@ -40,7 +41,7 @@
 // ------------------------------------------------------------------------------------------------
 // This section contains settings for the MQTT server you wish the device to connect to.
 
-#define MQTT_SERVER_IP IPAddress(192, 168, 0, 125)
+#define MQTT_SERVER_IP IPAddress(192, 168, 0, 208)
 #define MQTT_SERVER_PORT 1883
 
 #ifdef USE_NTP
@@ -63,7 +64,7 @@
 #define MQTT_FPP_STATUS_TOPIC "christmas/falcon/player/FPP/status"
 
 // This is the topic we will subscribe to for the purpose of determining the name of the current playlist.
-#define MQTT_FPP_PLAYLIST_NAME_TOPIC "christmas/falon/player/FPP/playlist/name/status"
+#define MQTT_FPP_PLAYLIST_NAME_TOPIC "christmas/falcon/player/FPP/playlist/name/status"
 
 // This is the topic we will subscribe to for the purpose of determining which song within the playlist is
 // currently playing.
@@ -73,20 +74,13 @@
 // currently playing.
 #define MQTT_FPP_PLAYLIST_SEQUENCE_STATUS_TOPIC "christmas/falcon/player/FPP/playlist/sequence/status"
 
-// When we start a rickroll, we play for 2 songs; my default is to have a pause playlist entry, and then to have the
-// media file play
-#define RICKROLL_SONG_COUNT 2
-
-// This is the topic we will publish to rick-roll unsuspecting guests who press the button outside of normal show hours.
-#define MQTT_FPP_RICKROLL_TOPIC "christmas/falcon/player/FPP/set/playlist/RickRoll/start"
-
 // We will publish push events to this topic when there is music playing; this can be used to get a
 // count of how many people press the button during your show.
-const char MQTT_BUTTON_PRESS_COUNT_PLAYING_TOPIC[] = "christmas/pressForMusic/countPlaying";
+const char MQTT_BUTTON_PRESS_COUNT_PLAYING_TOPIC[] = "christmas/pressForMusic2/countPlaying";
 
 // We will publish push events to this topic when there is NOT music playing.
 // This can be used to get a count of how many people push the button outside of the show.
-const char MQTT_BUTTON_PRESS_COUNT_IDLE_TOPIC[] = "christmas/pressForMusic/countIdle";
+const char MQTT_BUTTON_PRESS_COUNT_IDLE_TOPIC[] = "christmas/pressForMusic2/countIdle";
 
 // We will both publish and subscribe to this topic to allow multiple ESP8266-based "press for music" devices to
 // operate on the same network.  
@@ -98,9 +92,11 @@ const char MQTT_BUTTON_PRESS_COUNT_IDLE_TOPIC[] = "christmas/pressForMusic/count
 // relay for speakers on another ESP8266 device.
 // 2. Sync multiple "press for music" button/speaker pairings.  When one button is pressed all will turn on at the
 // same time.
-const char MQTT_BUTTON_TRIGGER_DEVICE_TOPIC[] = "christmas/pressForMusic/triggerDevice";
+const char MQTT_BUTTON_TRIGGER_DEVICE_TOPIC[] = "christmas/pressForMusic2/triggerDevice";
 
-const char MQTT_TRIGGER_PAYLOAD[] = "trigger";
+const char MQTT_TRIGGER_PAYLOAD[] = "{\"event_type\": \"press\"}";
+const String MQTT_TRIGGER_PAYLOAD_PREFIX = "{\"event_type\": \"press\", \"time\": ";
+const String MQTT_TRIGGER_PAYLOAD_SUFFIX = "\"}";
 
 // This is the topic we will public a signal to indicating that there was a trigger on your blueiris server.
 // Change undef to define if you want to do this type of publish.
@@ -133,26 +129,6 @@ const char MQTT_TRIGGER_PAYLOAD[] = "trigger";
 int SONG_COUNT = 2;
 
 // ------------------------------------------------------------------------------------------------
-// This section is for configuration of triggering another relay pin during times in your
-// playlist when no media is playing.
-// You may, for example, want to turn on a light during "Pause" entries in your playlist.
-// Note: This isn't perfect; FPP doesn't actually publish that a "pause" entry is being played,
-// so the closest proxy is to monitor for times when no media is playing.
-// By default this functionality is turned off
-
-#undef TRIGGER_RELAY_DURING_PAUSE
-
-// This is the pin on your ESP8266 which has a relay connected to it for the purpose of triggering
-// during media pauses.
-#define PAUSE_RELAY_PIN D2
-
-// This is how long AFTER a pause ends until the relay will be turned off again.
-// Unit is milliseconds.  The default value of 3000 means that 3 seconds after a pause and media
-// starts playing again, we'll turn the relay off.
-#define RELAY_OFF_AFTER_PAUSE_DURATION_MILLIS 3000
-
-
-// ------------------------------------------------------------------------------------------------
 // Constants.  Yay.
 
 // Status expected on the MQTT_FPP_STATUS_TOPIC topic when nothing is playing.
@@ -162,6 +138,7 @@ int SONG_COUNT = 2;
 
 #define DEBUG_SERIAL
 
+byte deviceMac[6];
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
 WiFiEventHandler wifiConnectHandler;
@@ -175,13 +152,6 @@ NTPClient timeClient(ntpUDP, NTP_SERVER_ADDRESS);
 
 // Whether we know FPP is playing music or not.
 bool isFppPlaying = false;
-bool isRickRollPending = false;
-bool isRickRolling = false;
-
-#define PAUSE_STATUS_PAUSED 0
-#define PAUSE_STATUS_UNPAUSED 1
-#define PAUSE_STATUS_UNPAUSE_SCHEDULED 2
-volatile int pauseStatus = PAUSE_STATUS_UNPAUSED;
 
 // File name for the preferences file which stores... the prefs!  Yay.
 #define PREFERENCES "/prefs.txt"
@@ -202,6 +172,14 @@ boolean overrideOn = false;
 unsigned long lastButtonStateChangeTime = 0;
 unsigned long debounceTime = 50;
 unsigned long deactivePauseRelayTime = 0;
+
+// Some things related to HA auto-discovery.
+WiFiClient wifiClient;
+HADevice haDevice(deviceMac, sizeof(deviceMac));
+HAMqtt mqtt(wifiClient, haDevice);
+HABinarySensor pressForMusicButton("press_for_music");
+HABinarySensor speakerStatus("speaker_status");
+HASwitch speakerForceOnState("speaker_force_on_state");
 
 // Used to track our own Ip address.
 String myIpAddress;
@@ -238,6 +216,25 @@ String getSongsOnPref() {
   return String(SONG_COUNT);
 }
 
+void forceSpeakersOn() {
+  #ifdef DEBUG_SERIAL
+    Serial.println("forceSpeakersOn");
+  #endif
+  overrideOn = true;
+  digitalWrite(SPEAKER_RELAY_PIN, HIGH);
+  speakerStatus.setState(true);
+  speakerForceOnState.setState(true);
+}
+
+void forceSpeakersOff() {
+  #ifdef DEBUG_SERIAL
+    Serial.println("forceSpeakersOff");
+  #endif
+  overrideOn = false;
+  deactiveSpeakers();
+  speakerForceOnState.setState(false);
+}
+
 // Shut off the speaker relay.
 void deactiveSpeakers() {
   #ifdef DEBUG_SERIAL
@@ -245,6 +242,7 @@ void deactiveSpeakers() {
   #endif
   if (!overrideOn) {
     digitalWrite(SPEAKER_RELAY_PIN, LOW);
+    speakerStatus.setState(false);
   }
   songsRemaining = -1;
 }
@@ -254,7 +252,22 @@ void connectToWifi() {
   #ifdef DEBUG_SERIAL
     Serial.println("Connecting to Wi-Fi...");
   #endif
+  // Get the WIFI mac; this is required for 
+  WiFi.macAddress(deviceMac);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  haDevice.setName("Push For Music Button");
+  haDevice.setManufacturer("Tyler Gunn");
+  haDevice.setSoftwareVersion("1.0.0");
+  pressForMusicButton.setCurrentState(isButtonPressed());
+  pressForMusicButton.setIcon("mdi:button-pointer");
+  pressForMusicButton.setName("Press For Music");
+  speakerStatus.setCurrentState(false);
+  speakerStatus.setIcon("mdi:toggle-switch-variant");
+  speakerStatus.setName("Speaker State");
+  speakerForceOnState.setCurrentState(false);
+  speakerForceOnState.setIcon("mdi:toggle-switch-variant");
+  speakerForceOnState.setName("Speaker Force On");
+  speakerForceOnState.onCommand(onSwitchCommand);
 }
 
 void saveSongCountPref() {
@@ -269,6 +282,25 @@ void saveSongCountPref() {
   #ifdef DEBUG_SERIAL
     Serial.println("No preference found; created");
   #endif
+}
+
+
+// Handle incoming switch activations mentioned from Home Assistant.
+void onSwitchCommand(bool state, HASwitch* sender)
+{
+    if (sender == &speakerForceOnState) {
+        #ifdef DEBUG_SERIAL
+          if (state) {
+            Serial.println("Home Assistant asked for speakers on");
+            forceSpeakersOn();
+          } else {
+            Serial.println("Home Assistant asked for speakers off");
+            forceSpeakersOff();
+          }
+        #endif
+        // Ack back to HA since we'll do what we are asked.
+        speakerForceOnState.setState(state);
+    }
 }
 
 // Async callback received when the wifis are up; connect to MQTT at this point.
@@ -315,15 +347,13 @@ void onWifiConnect(const WiFiEventStationModeGotIP& event) {
 
   // Turn speakers on!
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    overrideOn = true;
-    digitalWrite(SPEAKER_RELAY_PIN, HIGH);    
+    forceSpeakersOn();
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
 
   // Turn speakers off!
   server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    overrideOn = false;
-    deactiveSpeakers();
+    forceSpeakersOff();
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
 
@@ -375,9 +405,6 @@ void onMqttConnect(bool sessionPresent) {
   #endif
   uint16_t packetIdSub = mqttClient.subscribe(MQTT_FPP_STATUS_TOPIC, 2);
   packetIdSub = mqttClient.subscribe(MQTT_FPP_PLAYLIST_POSITION_TOPIC, 2);
-  #ifdef TRIGGER_RELAY_DURING_PAUSE
-  packetIdSub = mqttClient.subscribe(MQTT_FPP_PLAYLIST_SEQUENCE_STATUS_TOPIC, 2);
-  #endif
 
   // Track triggers from all devices.
   packetIdSub = mqttClient.subscribe(MQTT_BUTTON_TRIGGER_DEVICE_TOPIC, 2);
@@ -427,30 +454,13 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
         Serial.println("  << FPP is idle");
       #endif
       isFppPlaying = false;
-      isRickRolling = false;
       currentPlaylistPosition = -1;
       deactiveSpeakers();
-      #ifdef TRIGGER_RELAY_DURING_PAUSE
-        if (pauseStatus != PAUSE_STATUS_UNPAUSED) {
-          pauseStatus = PAUSE_STATUS_UNPAUSED;
-          digitalWrite(PAUSE_RELAY_PIN, LOW);
-          #ifdef DEBUG_SERIAL
-            Serial.println("  Turning off pause relay.");
-          #endif
-        }
-      #endif
     } else if (strncmp(payload, FPP_STATUS_PLAYING, len) == 0) {
       #ifdef DEBUG_SERIAL
         Serial.println("  << FPP Is playing");
       #endif
       isFppPlaying = true;
-
-      if (isRickRollPending) {
-        songsRemaining = RICKROLL_SONG_COUNT + 1;
-        digitalWrite(SPEAKER_RELAY_PIN, HIGH);
-        isRickRollPending = false;
-        isRickRolling = true;
-      }
     }
   // Change in playlist position
   } else if (strcmp(topic, MQTT_FPP_PLAYLIST_POSITION_TOPIC) == 0) {
@@ -495,23 +505,7 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     Serial.print(tempMedia);
     Serial.println("|");
     #endif
-    bool isSequenceNowPaused = (strlen(tempMedia) == 0);
-    if (pauseStatus == PAUSE_STATUS_UNPAUSED && isSequenceNowPaused) {
-      // No media name, so assume media is paused; turn on the relay
-      pauseStatus = PAUSE_STATUS_PAUSED;
-      digitalWrite(PAUSE_RELAY_PIN, HIGH);
-      #ifdef DEBUG_SERIAL
-        Serial.println("  Media is paused; turn on relay.");
-      #endif
-    } else if (pauseStatus == PAUSE_STATUS_PAUSED && !isSequenceNowPaused) {
-      // In the main loop we will turn off the relay after we hit this timeout.
-      pauseStatus = PAUSE_STATUS_UNPAUSE_SCHEDULED;
-      deactivePauseRelayTime = millis() + RELAY_OFF_AFTER_PAUSE_DURATION_MILLIS;
-      #ifdef DEBUG_SERIAL
-        Serial.print("  Media unpause; schedule relay off at ");
-        Serial.println(deactivePauseRelayTime);
-      #endif
-    }
+
   // Monitor button presses on other devices (and our own).
   } else if (strcmp(topic, MQTT_BUTTON_TRIGGER_DEVICE_TOPIC) == 0) {
     char tempIp[len + 1];
@@ -520,7 +514,7 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
 
     if (strcmp(tempIp, myIpAddress.c_str()) == 0) {
       #ifdef DEBUG_SERIAL
-        Serial.println(" Button presss; local ignored.");
+        Serial.println(" Button press received via MQTT; local ignored.");
       #endif
     } else {
       #ifdef DEBUG_SERIAL
@@ -581,12 +575,8 @@ void setup() {
   // Let's just be sure that the relay is off on boot.
   digitalWrite(SPEAKER_RELAY_PIN, LOW);
   
-  #ifdef TRIGGER_RELAY_DURING_PAUSE
-  pinMode(PAUSE_RELAY_PIN, OUTPUT);
-  digitalWrite(PAUSE_RELAY_PIN, LOW);
-  #endif
-
-  pinMode(SWITCH_PIN, INPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  newButtonState = digitalRead(SWITCH_PIN);
   
   Serial.begin(115200);
   Serial.println();
@@ -603,41 +593,17 @@ void setup() {
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
 
+  mqtt.begin(MQTT_SERVER_IP);
+
   SPIFFS.begin();
   initializeFilesystem();
 
   connectToWifi();
 }
 
-void handleRickRoll() {
-
-  #ifdef USE_NTP
-    // Since ESP8266 doesn't have a realtime clock, we need to query NTP for the time/date.
-    timeClient.update();
-
-    unsigned long epochTime = timeClient.getEpochTime();
-    #ifdef DEBUG_SERIAL
-    Serial.print("Epoch Time: ");
-    Serial.println(epochTime);
-    #endif
-    struct tm *timeinfo = gmtime ((time_t *)&epochTime);
-
-    if (timeinfo->tm_hour < 6 || timeinfo->tm_hour > 22) {
-      #ifdef DEBUG_SERIAL
-        Serial.println("SKipped rickroll; out of valid hours." );
-      #endif
-      return;
-    }
-    
-  #endif
-  isRickRollPending = true;
-  isRickRolling = false;
-  mqttClient.publish(MQTT_FPP_RICKROLL_TOPIC, 2, true, "start");
-}
-
 void handleButtonDown(bool publishPress) {
   const char* topic;
-  const char* payload;
+  
 
   #ifdef USE_NTP
     // Since ESP8266 doesn't have a realtime clock, we need to query NTP for the time/date.
@@ -651,16 +617,17 @@ void handleButtonDown(bool publishPress) {
     struct tm *timeinfo = gmtime ((time_t *)&epochTime);
     char dateString[80];
     strftime(dateString,80,"%Y-%m-%dT%H:%M:%S", timeinfo);
+    String dateStringObj = dateString;
     #ifdef DEBUG_SERIAL
     Serial.print("Formatted time: ");
     Serial.println(dateString);
     #endif
-    payload = dateString;
+    String payload = "{\"event_type\": \"press\", \"time\":\"" + dateStringObj + "\"}";
   #else
-    payload = MQTT_TRIGGER_PAYLOAD;
+    String payload = MQTT_TRIGGER_PAYLOAD;
   #endif
 
-  if (isFppPlaying && !isRickRolling) {
+  if (isFppPlaying) {
     songsRemaining = SONG_COUNT;
     #ifdef DEBUG_SERIAL
       Serial.print("Activate speakers - songs remaining: ");
@@ -672,22 +639,19 @@ void handleButtonDown(bool publishPress) {
       // Only notify BlueIris if the press originated on this device to avoid over-triggering
       // the cameras
       if (publishPress) {
-        mqttClient.publish(MQTT_BLUEIRIS_TRIGGER_TOPIC, 2, true, MQTT_BLUEIRIS_TRIGGER_PAYLOAD);
+        mqttClient.publish(MQTT_BLUEIRIS_TRIGGER_TOPIC, 2, false, MQTT_BLUEIRIS_TRIGGER_PAYLOAD);
       }
     #endif
   } else {
-    // TODO: Rickroll someone who pushes the button during the day when the show is off.
-    // I'm envisioning this will turn on the speakers and publish to FPP to start a playlist with
-    // 'ol Rick on it.
     #ifdef DEBUG_SERIAL
-      Serial.println("  << Rick-roll");
+      Serial.println("  << idle press");
     #endif
-    handleRickRoll();
     topic = MQTT_BUTTON_PRESS_COUNT_IDLE_TOPIC;
   }
   // Only publish presses on the original triggering devices to avoid overstating presses.
   if (publishPress) {
-    mqttClient.publish(topic, 2, true, payload);
+    speakerStatus.setState(true);
+    mqttClient.publish(topic, 2, false, payload.c_str());
   }
 
   // Let other devices know that the button was pressed, but only if we're not triggering because another
@@ -696,8 +660,12 @@ void handleButtonDown(bool publishPress) {
     #ifdef DEBUG_SERIAL
       Serial.println("  Publishing local press");
     #endif
-    mqttClient.publish(MQTT_BUTTON_TRIGGER_DEVICE_TOPIC, 2, true, myIpAddress.c_str());
+    mqttClient.publish(MQTT_BUTTON_TRIGGER_DEVICE_TOPIC, 2, false, myIpAddress.c_str());
   }
+}
+
+boolean isButtonPressed() {
+  return newButtonState == LOW;
 }
 
 // The main event loop.
@@ -712,28 +680,20 @@ void loop() {
 
   // Only handle the state change if it ocurred after the debounce time.
   if ((millis() - lastButtonStateChangeTime) > debounceTime && newButtonState != currentButtonState) {
-    if (newButtonState == HIGH) {
+    if (isButtonPressed()) {
       #ifdef DEBUG_SERIAL
         Serial.println("  << BUTTON DOWN");
       #endif
       handleButtonDown(true);
+      pressForMusicButton.setState(true);
     } else {
       #ifdef DEBUG_SERIAL
         Serial.println("  << BUTTON UP");
       #endif
+      pressForMusicButton.setState(false);
     }
     currentButtonState = newButtonState;
   } 
   lastButtonState = newButtonState;
-
-  #ifdef TRIGGER_RELAY_DURING_PAUSE
-    if (isFppPlaying && pauseStatus == PAUSE_STATUS_UNPAUSE_SCHEDULED && millis() > deactivePauseRelayTime) {
-      #ifdef DEBUG_SERIAL
-        Serial.print("  Media unpause relay off at ");
-        Serial.println(millis());
-      #endif
-      pauseStatus = PAUSE_STATUS_UNPAUSED;
-      digitalWrite(PAUSE_RELAY_PIN, LOW);
-    }
-  #endif
+  mqtt.loop();
 }
